1e22b095c0f15ee7f95fbb1015f0050fe5f70d0e
rishavbhowmik <53208265+rishavbhowmik@users.noreply.github.com>
2020-08-08 22:58:38 +0530

    Update source_Aug_2020.js

--- BigNumber/DEV_CODES/NodeJS/BigPositiveInteger/source_Aug_2020.js
+++ BigNumber/DEV_CODES/NodeJS/BigPositiveInteger/source_Aug_2020.js
@@ -83,7 +83,9 @@ class BigPositiveInteger{
 
         }
 
-        return res.reverse().join('')
+        if(carry[i]>0){res.push(carry[i]);}
+
+        return res.reverse().join('').replace(/(0)*/,"")
     }
 
     static substract(n1, n2){
@@ -116,7 +118,7 @@ class BigPositiveInteger{
                 
                 res.push(u - Number(n2[i] || 0))
             }
-            return res.reverse().join('')
+            return res.reverse().join('').replace(/(0)*/,"")
         }
         else{
             //when n1<n2
@@ -138,6 +140,60 @@ class BigPositiveInteger{
         }
 
     }
+
+    static mul_1d(n1, n2){
+        if((typeof n1 != 'string')||(typeof n2 != 'number')||( n2 > 10)){
+            throw Error("Bad Input")
+        }
+        n1 = n1.split('').reverse()
+
+        var carry = [0], res = [];
+        var i;
+        for(i=0; i<n1.length;i++){
+            
+            const a10 = Number(n1[i] || 0) * n2 + carry[i]
+
+            if(a10 > 9){
+                carry.push( Number(`${a10}`[0] ))
+                res.push( `${a10}`[1] )
+            }else{
+                carry.push( 0 )
+                res.push(a10)
+            }
+        }
+
+        if(carry[i]>0){res.push(carry[i]);}
+
+        return res.reverse().join('').replace(/(0)*/,"")
+    }
+
+    static mul(n1, n2){
+        if((typeof n1 != 'string')||(typeof n2 != 'string')){
+            throw Error("Bad Input")
+        }
+
+        if(n1.length<n2.length){
+            const t = n2; n2 = n1; n1 = t
+        }
+
+        n2 = n2.split('').reverse()
+
+        var digs = []
+
+        for(var i=0; i<n2.length; i++){
+            const node = BigPositiveInteger.mul_1d(n1, Number(n2[i])) + Array(i+1).join("0")
+            digs.push(node)
+        }
+
+        var out = "0"
+
+        for(var i=0; i<digs.length; i++){
+            out = BigPositiveInteger.add(out, digs[i])
+        }
+
+        return out;
+
+    }
 }
 
 
@@ -153,3 +209,8 @@ class BigPositiveInteger{
 //console.log(`Sub : ${BigPositiveInteger.substract('1551231239920808234080982309823098234098234098230498230948230999999', '32991329913299132991')}`)
 
 //console.log(`Sub : ${BigPositiveInteger.substract('122319982398408923879872398723987239847987298732480720348098092834898723984798723948732991329913299132991', '15512312399208082340809823098230982340982340982304982309482309999991551231239920808234080982309823098234098234098230498230948230999999')}`)
+
+//console.log(`Sub : ${BigPositiveInteger.substract('1992','1891')}`)
+//console.log(`Sum : ${BigPositiveInteger.add("9989","3121")}`)
+//console.log(`Mul 1D: ${BigPositiveInteger.mul_1d("123",8)}`)
+//console.log(`Mul: ${BigPositiveInteger.mul("9991180723087407234098230984092834098234989898981","98087234097230972039740972309723407")}`)
